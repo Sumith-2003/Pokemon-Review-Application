9ddccc277e4b04a1854009f23c9ae5f336367063
Sumith <sumith07devadiga@gmail.com>
2025-06-23 23:59:47 +0530

    Ran Add-Migration InitialCreate to add migration

---
+++ Repository/PokemonRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReviewApp.Data;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context) 
+        {
+            _context = context; 
+        }
+    }
+}

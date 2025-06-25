815bd6e51d59412b1cef1f7b9b52878df50502c6
Sumith <sumith07devadiga@gmail.com>
2025-06-26 00:25:55 +0530

    Added few GET requests and created pokemon dto

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -17,6 +17,7 @@ namespace PokemonReviewApp.Controllers
 
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        [ProducesResponseType(500)]
         public IActionResult GetPokemons()
         {
             var pokemons = _pokemonRepository.GetPokemons();
@@ -26,5 +27,28 @@ namespace PokemonReviewApp.Controllers
             }
             return Ok(pokemons);
         }
+
+        [HttpGet("{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int pokeId) 
+        {
+            if(!_pokemonRepository.PokemonExists(pokeId)) return NotFound();
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var pokemon = _pokemonRepository.GetPokemon(pokeId);
+            return Ok(pokemon);
+        }
+
+        [HttpGet("{pokeId}/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonRating(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeId)) return NotFound();
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var rating = _pokemonRepository.GetPokemonRating(pokeId);
+            return Ok(rating);
+        }
+
     }
 }
--- Repository/PokemonRepository.cs
+++ Repository/PokemonRepository.cs
@@ -7,14 +7,38 @@ namespace PokemonReviewApp.Repository
     public class PokemonRepository : IPokemonRepository
     {
         private readonly DataContext _context;
-        public PokemonRepository(DataContext context) 
+        public PokemonRepository(DataContext context)
         {
-            _context = context; 
+            _context = context;
+        }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemon
+                .Where(p => p.Name.Trim().ToLower() == name.Trim().ToLower())
+                .FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeid)
+        {
+            return (decimal) _context.Reviews
+                .Where(r => r.Pokemon.Id == pokeid)
+                .Average(r => r.Rating);
         }
 
         public ICollection<Pokemon> GetPokemons()
         {
             return _context.Pokemon.OrderBy(p => p.Name).ToList();
+        }
+
+        public bool PokemonExists(int id)
+        {
+            return _context.Pokemon.Any(p => p.Id == id);
         }
     }
 }

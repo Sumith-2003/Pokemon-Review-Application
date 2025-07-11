7e22f169db3da67a887d0bd32c1119b62d22693b
Sumith <sumith07devadiga@gmail.com>
2025-07-11 23:40:14 +0530

    Rolled back the in memory caching changes

--- Repositories/Implementations/CategoryRepository.cs
+++ Repositories/Implementations/CategoryRepository.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Caching.Memory;
 using PokemonReviewApp.Data;
 using PokemonReviewApp.Models;
 using PokemonReviewApp.Repositories.Interfaces;
@@ -9,20 +8,13 @@ namespace PokemonReviewApp.Repositories.Implementations
     public class CategoryRepository : ICategoryRepository
     {
         private readonly DataContext _context;
-        // IMemoryCache instance for implementing in-memory caching.
-        private readonly IMemoryCache _cache;
-        // Cache expiration time set to 30 minutes.
-        private readonly TimeSpan _cacheExpiration = TimeSpan.FromMinutes(30);
 
-        public CategoryRepository(DataContext context, IMemoryCache cache)
+        public CategoryRepository(DataContext context)
         {
             _context = context;
-            _cache = cache;
         }
-        public async Task<bool> CategoryExists(int categoryId)
-        {
-            return await _context.Categories.AnyAsync(c => c.Id == categoryId);
-        }
+        public async Task<bool> CategoryExists(int categoryId) =>
+            await _context.Categories.AnyAsync(c => c.Id == categoryId);
 
         public async Task<bool> CreateCategory(Category category)
         {
@@ -31,26 +23,19 @@ namespace PokemonReviewApp.Repositories.Implementations
             //Save changes to the database
             return await Save();
         }
-        public async Task<ICollection<Category>> GetCategories()
-        {
+        public async Task<ICollection<Category>> GetCategories() =>
+            await _context.Categories.OrderBy(c => c.Id).ToListAsync();
 
-            return await _context.Categories.OrderBy(c => c.Id).ToListAsync();
-        }
+        public async Task<Category> GetCategory(int categoryId) =>
+            await _context.Categories
+                          .Where(c => c.Id == categoryId)
+                          .FirstAsync();
 
-        public async Task<Category> GetCategory(int categoryId)
-        {
-            return await _context.Categories
-                        .Where(c => c.Id == categoryId)
-                        .FirstAsync();
-        }
-
-        public async Task<ICollection<Pokemon>> GetPokemonsByCategory(int categoryId)
-        {
-            return await _context.Pokemon
-                .Where(p => p.PokemonCategories.Any(pc => pc.Category.Id == categoryId))
-                .OrderBy(p => p.Name)
-                .ToListAsync();
-        }
+        public async Task<ICollection<Pokemon>> GetPokemonsByCategory(int categoryId) =>
+            await _context.Pokemon
+                          .Where(p => p.PokemonCategories.Any(pc => pc.Category.Id == categoryId))
+                          .OrderBy(p => p.Name)
+                          .ToListAsync();
 
         public async Task<bool> Save()
         {
